6acd4486cd18b0a86a8166f1e163493b5a3b055a
Ben Brew <benmbrew@gmail.com>
2015-12-15 12:34:45 -0500

    map to genome

---
+++ Scripts/probe_to_genome.R
@@ -0,0 +1,35 @@
+##### This script looks at different ways in which you can map probe level methylation data to 
+# genome level using a variety of R packages..49/34
+
+library(minfi)
+library(affy)
+library(ArrayExpress)
+
+# Initialize folders
+home_folder <- '/home/benbrew/Documents'
+project_folder <- paste(home_folder, 'LFS', sep = '/')
+data_folder <- paste(project_folder, 'Data', sep = '/')
+
+# set working directory and load data 
+setwd(data_folder)
+load('cleaned.RData')
+
+# transopse methylation data so that probes are rows and columns are ids
+methylation_raw <- as.data.frame(t(methylation_raw), stringsAsFactors = FALSE)
+
+# Make the first row column names
+colnames(methylation_raw) <- methylation_raw[1,]
+data <- methylation_raw <- methylation_raw[-1,]
+# data <- cbind(id = rownames(methylation_raw), methylation_raw)
+# rownames(data) <- NULL
+data <- data[, unique(colnames(data))]
+
+# apply the minfi functions to transform data 
+# object = Either a MethylSet, a RGChannelSet or a RatioSet.
+
+data <- data[1:100,]
+annot_data <- AnnotatedDataFrame(data)
+
+getLocations(annot_data)
+
+
