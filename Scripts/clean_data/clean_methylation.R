b956b8c6cdeb91dc6a0826f3a22960d96a22dce4
Ben Brew <benmbrew@gmail.com>
2016-01-05 11:41:58 -0500

    reorganized and joining by probe names

---
+++ Scripts/clean_data/clean_methylation.R
@@ -0,0 +1,97 @@
+## Script for reading, cleaning, and combining methylation data.
+
+# Initialize folders
+home_folder <- '/home/benbrew/Documents'
+project_folder <- paste0(home_folder, '/LFS')
+data_folder <- paste0(project_folder, '/Data')
+methyl_data <- paste0(data_folder, '/methyl_data')
+data_name <- '/Chr'
+
+#################################################################################
+# Take Nardine's methyalation data and clean the column names
+#################################################################################
+
+# Read in methylation data 
+methylation24 <- read.table(paste(data_folder,'/methyl.txt', sep = ''), header = TRUE)
+methylation17 <- read.csv(paste(data_folder, '/methyl_17.csv', sep = ''), header = TRUE)
+
+# This function takes the mehtylation data and removes the 'X' from the colnames 
+
+cleanColNames <- function(data){
+  for(i in 1:ncol(data)){
+    sub_dat <- colnames(data)[i]
+    if(grepl('X', sub_dat)){
+      split <- strsplit(sub_dat, 'X')
+      last_split <- lapply(split, function(x) x[length(x)])
+      colnames(data)[i] <- unlist(last_split)
+    }else{
+      colnames(data)[i] <- sub_dat
+    }
+  }
+  return(data)
+}
+
+methylation24 <- cleanColNames(methylation24)
+methylation17 <- cleanColNames(methylation17)
+
+##############################################################################
+# Read in raw methylation data from Nardine and 
+# Combine the list and put in same format as previous 
+# methylation with probes in first column and IDs in the following columns
+##############################################################################
+
+# Read in raw mehtylation data
+num_sets <- 23 
+methylation_raw <-  vector('list', num_sets)
+
+# read in raw methylation files. 
+for(i in (1:num_sets)){
+  methylation_raw[[i]] <- read.delim(paste(data_folder, data_name, i, '.txt', sep = ''), header = TRUE)
+}
+
+# iterate through each data set and retrieve the IDs then combine them into one data table in same format as 
+# Previous methylation data
+
+for (i in 1:num_sets){
+  sub_dat <- methylation_raw[[i]]
+  column_split <- strsplit(as.character(sub_dat$X), '#')
+  last_digits <- lapply(column_split, function(x) x[length(x)])
+  sub_ids <- unlist(last_digits)
+  sub_ids <- gsub('RD-', '', sub_ids)
+  methylation_raw[[i]][,1] <- sub_ids
+  if(i > 1){
+    methylation_raw[[i]] <- methylation_raw[[i]][, -1]
+  }
+}
+methylation <- do.call('cbind', methylation_raw)
+methylation <- as.data.frame(t(methylation), stringsAsFactors = FALSE)
+methylation <- cbind(x = rownames(methylation), methylation)
+colnames(methylation) <- methylation[1,]
+methylation <- methylation[-1,]
+names(methylation)[1] <- 'Probe'
+rownames(methylation) <- NULL  
+
+
+cleanProbe <- function(data){
+  for(i in 2:ncol(data)){
+    data[,i] <- as.numeric(data[,i])
+  }
+  data <- data[!grepl('ch', data$Probe),]
+  data <- data[!grepl('_', data$Probe),]
+  colnames(data) <- gsub('-', '_', colnames(data))
+  data <- data[data$Probe != 'X.1',]
+  rownames(data) <- NULL
+  
+
+return(data)
+}
+
+methylation <- cleanProbe(methylation)
+
+# write methylation_raw to methyl_data folder as csv
+write.csv(methylation, paste(methyl_data, '/methylation.csv', sep = ''), row.names= FALSE)
+
+
+
+
+
--- Scripts/clean_data/probe_to_genome.R
+++ Scripts/clean_data/probe_to_genome.R
@@ -1,24 +1,23 @@
 ##### This script maps cgp probe sites to nearest genome using the fdb.infiniumMethylation.hg19 data
 # base, and primarily, the getNearestGene function.
 
-library(minfi)
-library(affy)
 library(FDb.InfiniumMethylation.hg19)
 library(dplyr)
+
 # Initialize folders
 home_folder <- '/home/benbrew/Documents'
 project_folder <- paste0(home_folder, '/LFS')
 data_folder <- paste0(project_folder, '/Data')
+methyl_data <- paste0(data_folder, '/methyl_data')
 
-# set working directory and load data 
-setwd(data_folder)
-load('cleaned.RData')
+# Read in methylation data 
+methylation <- read.csv(paste0(methyl_data, '/methylation.csv'), header = TRUE, check.names = FALSE)
 
 # Load the 450k data from hg19 (bioconductor, library is FDb.InfiniumMethylation.hg19)
 hm450 <- get450k()
 
 # Get probe names from our methylation data  
-probe_names <- as.character(methylation$probe)
+probe_names <- as.character(methylation$Probe)
 
 # remove probes that have less than 10 characters.
 probe_names <- probe_names[nchar(probe_names) ==10]
@@ -32,7 +31,7 @@ probe_info <- cbind(probe = rownames(probe_info), probe_info)
 rownames(probe_info) <- NULL
 
 # join probe_info with methylation. This keeps all of the probes that we could match in hm450 and drops the others.
-methyl_gene <- left_join(probe_info, methylation, by = 'probe')
+methyl_gene <- left_join(probe_info, methylation, by = c('probe'= 'Probe'))
 
 # Get rid of extra variables.
 methyl_gene$probe <- NULL
