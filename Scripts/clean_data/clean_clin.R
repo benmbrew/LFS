6b3b0ea5dfa104db622642e8e367b93115df591b
Ben Brew <benmbrew@gmail.com>
2016-01-31 20:46:34 -0500

    new clinical data

--- Scripts/clean_data/clean_clin.R
+++ Scripts/clean_data/clean_clin.R
@@ -74,3 +74,170 @@ clin$age_fac <- ifelse(clin$age_of_onset > 5, 'older_5', 'younger_5')
 
 # write clin to data_folder so it can be loaded to database
 write.csv(clin, paste(clin_data,'clinical.csv', sep ='/'), row.names = FALSE)
+
+#######################################################################
+# clean clinical data sent from Ana on 1/29/2016 
+library(xlsx)
+library(gsubfn)
+clin1 <- read.xlsx(paste0(clin_data, '/malkin_jan_29th.xlsx'), 1, stringsAsFactors = FALSE) # read the first sheet
+clin2 <- read.xlsx(paste0(clin_data, '/malkin_jan_29th.xlsx'), 2, stringsAsFactors = FALSE) # read the second sheet
+
+# For the time being drop family from clin1 
+clin1$NA. <- NULL
+
+# combine the two data sets 
+clin <- rbind(clin1, clin2)
+
+# remove all columns that are completely NA 
+clin <- clin[, colSums(is.na(clin)) < nrow(clin)]
+
+# remove all rows that are completely NA
+clin <- clin[rowSums(is.na(clin)) < ncol(clin),]
+
+#########################################
+# make column names lower case and remove any '.' and replace with '_'
+cleanColNames <- function(data) {
+  col_names <- colnames(data)
+  col_names <- tolower(col_names)
+    if (grepl('.', col_names)) {
+      col_names <- gsub("([.])\\1+", "_", col_names)
+      col_names <- gsub('.', '_', col_names, fixed = TRUE)
+  }
+  colnames(data) <- col_names
+  return(data)
+}
+
+clin <- cleanColNames(clin)
+
+
+# clean NAs and N/As
+clin <- as.data.frame(apply(clin, 2, function(x) gsub('N/A', 'NA', x)))
+
+# clean white spaces 
+clin <- as.data.frame(apply(clin, 2, function(x) gsub('\\s+', '', x)))
+
+##############################################
+# take any rows with double samples and split them into the number of rows equal to the number of samples.
+
+splitRows <- function(data, duplicate_table){
+  
+  for (i in 1:nrow(data)) {
+    sub_data <- data[i,]
+    
+    if (grepl('/', sub_data$blood_dna_malkin_lab_)) {
+      split_malkin <- strsplit(as.character(sub_data$blood_dna_malkin_lab_), '/')
+      split_age <- strsplit(as.character(sub_data$age_sample_collection), '/')
+      split_malkin <- cbind(unlist(split_malkin))
+      split_age <- cbind(unlist(split_age))
+      duplicate <- cbind(split_malkin, split_age, sub_data)
+      duplicate_table <- rbind(duplicate_table, duplicate)
+    }
+  }
+  
+  data <- data[!grepl('/', data$blood_dna_malkin_lab_),]
+  duplicate_table$blood_dna_malkin_lab_ <- NULL
+  duplicate_table$age_sample_collection <- NULL
+  colnames(duplicate_table)[1:2] <- c('blood_dna_malkin_lab_', 'age_sample_collection')
+  duplicate_table <- duplicate_table[, colnames(data)]
+  data <- rbind(duplicate_table, data)
+  return(data)
+  
+}
+
+empty_table <- data.frame(matrix(ncol = ncol(clin), nrow = 0))
+clin <- splitRows(clin, empty_table)
+
+# Clean age of diagnosis column
+clin$age_diagnosis <- as.character(clin$age_diagnosis)
+clin$age_sample_collection <- as.character(clin$age_sample_collection)
+
+##########################################################
+# Clean the age variable so that is expressed in years 
+
+cleanAge <- 
+  
+  function(data, column_name) {
+  
+  age_vector <- data[, column_name]
+    
+  for(i in 1:length(age_vector)){
+      
+    temp_age <- age_vector[i]
+      
+      if (grepl('~', temp_age, fixed = TRUE)){
+        temp_age <- strsplit(temp_age, '~', fixed = TRUE)
+        temp.2_age <- unlist(temp_age)
+        temp.3_age <- temp.2_age[2]
+        temp_age <- temp.3_age
+      }
+      if (grepl('(', temp_age, fixed = TRUE) || grepl(')', temp_age, fixed = TRUE)){
+        temp_age <- strsplit(temp_age, '(', fixed = TRUE)
+        temp.2_age <- unlist(temp_age)
+        temp.3_age <- temp.2_age[1]
+        temp_age <- temp.3_age
+      }
+     
+      if (grepl('>', temp_age) || grepl('<', temp_age)) {
+        temp_age <- gsub('<', '' , temp_age)
+        temp_age <- gsub('>', '', temp_age)
+      }
+      
+      if (grepl('k', temp_age)) {
+        temp_age <- NA
+      }
+      
+      if (grepl('NA', temp_age)) {
+        temp_age <- NA
+      }
+      
+      if (grepl('y', temp_age) && grepl('m', temp_age)) {
+        temp_age <- gsubfn('([y,m])', list('y' = '.', 'm' = ''), as.character(temp_age))
+        temp.2_age <- strsplit(temp_age, '.', fixed = TRUE)
+        temp.3_age <- do.call('rbind', temp.2_age)
+        temp.4_age <- gsub('.*\\.', paste0(temp.3_age[1], '.') , as.numeric(temp.3_age[2])/12)
+        temp_age <- temp.4_age
+   
+        } else if (grepl('y', temp_age) && !grepl('m', temp_age)) {
+        temp_age  <- gsubfn('y', '.0', as.character(temp_age))
+    
+        } else if (!grepl('y', temp_age) && grepl('m', temp_age) && !grepl('w', temp_age)) {
+        temp_age <- gsubfn('m', '', as.character(temp_age))
+        temp_age <- as.numeric(temp_age)/12
+    
+        } else if (grepl('w', temp_age) && grepl('m', temp_age)) {
+        temp_age <- gsubfn('([m,w])', list('m' = '.', 'w' = ''), as.character(temp_age))
+        temp_age <- ceiling(as.numeric(temp_age))
+   
+        } else if (grepl('w', temp_age) && !grepl('m', temp_age)) {
+        temp_age <- gsub('w', '', temp_age)
+    } 
+     
+    age_vector[i] <- temp_age
+  
+  }
+  
+  data[, column_name] <- age_vector
+  return(data)
+
+} 
+
+clin <- cleanAge(clin, column_name = 'age_diagnosis')
+clin <- cleanAge(clin, column_name = 'age_sample_collection')
+
+
+############################################################
+# clean date column and create variable for patient age 
+clin$dob <- ifelse(grepl('unknown', as.character(clin$dob)), NA, 
+                  ifelse(grepl('notknown', as.character(clin$dob)), NA, as.character(clin$dob)))
+  
+# extract only the last four characters to get year of birth
+
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+
+clin$yob <- as.numeric(substrRight(as.character(clin$dob), 4))
+clin$age <- 2016 - clin$yob
+
+############################################################
+
