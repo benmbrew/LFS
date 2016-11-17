d4318d3d7973fe5990abde4d3537296a4da3d003
benmbrew <benmbrew@gmail.com>
2016-11-16 19:56:53 -0500

    control models

--- Scripts/predict_age/check_controls.R
+++ Scripts/predict_age/check_controls.R
@@ -24,8 +24,30 @@ rm(beta_raw, beta_quan, beta_swan, beta_funnorm)
 load(paste0(idat_data, '/imputed_idat_betas_final.RData'))
 
 ##########################
+# see how many of original data is in control data
+#########################
+length(which(beta_raw$id %in% beta_raw_control$id))
+# there are 12 samples in beta_raw that are in beta_raw_control
+
+# how many of beta_raw are mut and unaffected
+temp <- beta_raw[beta_raw$p53_germline == 'Mut',]
+length(which(is.na(temp$age_diagnosis)))
+# there are 31 mut no cancers in beta_raw, 12 of which in are controls, 
+# leaving an extra 19 that could be added to control data
+
+##########################
 # find the 3 controls that are both in 450 and 850
 ###########################
+
+# subset control data and original data by 3 samples in both
+subsetBeta <- function(control, orig) {
+  control <- control[control$cancer_diagnosis_diagnoses != 'Unaffected',]
+  control_ids <- paste(control$id, collapse = '|')
+  orig <- orig[grepl(control_ids, orig$id), ]
+  return(list(control, orig))
+}
+
+
 
 # see if the values are the same 
 
